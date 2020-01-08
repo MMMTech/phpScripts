bb890c6592244352290859c90b30ffe2162b8822
Macario Lopez <macariolopez999@hotmail.com>
2020-01-08 12:58:10 +0100

    Improved telnet server...visual imprvements... plus added functionality (added commands like indir for list content of server directory to client and help command to display help menu to client)

--- telnetServer.php
+++ telnetServer.php
@@ -1,5 +1,26 @@
 #!/usr/local/bin/php -q
 <?php
+
+//Menu functionality
+function printMenu(){
+	
+	return "\nOUTPUT: \n"
+		. "1. Type: help    --- for this Menu.\n"
+		. "2. Type: indir   --- listing current working directory on serverside\n";
+}
+
+
+//Formatting output
+function setOutput($out){
+
+	return "\nOUTPUT: \n"
+	. "===================================\n"
+	. "$out\n"
+	. "===================================\n"
+	. "\nCLIENT:>>>";	
+}
+
+
 error_reporting(E_ALL);
 
 /* Allow the script to hang around waiting for connections. */
@@ -9,8 +30,20 @@ set_time_limit(0);
  * as it comes in. */
 ob_implicit_flush();
 
+
+$recv_port = 2048;
 $address = $argv[1];
-$port = intval($argv[2]);
+$port =  intval($argv[2]);
+
+//Testing if Cmdline cariables are set
+//if(!isset($argv[1])){
+
+//	echo "Argument vector 1 set";
+
+//}
+
+
+$output = '';
 
 if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
     echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
@@ -24,18 +57,27 @@ if (socket_listen($sock, 5) === false) {
     echo "socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
 }
 
+
+
 do {
     if (($msgsock = socket_accept($sock)) === false) {
         echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
         break;
     }
+
+    
+
     /* Send instructions. */
-    $msg = "\nWelcome to the PHP Test Server. \n" .
-        "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
-    socket_write($msgsock, $msg, strlen($msg));
+    $msg = "\nWelcome to the PHP Telnet Server. \n" .
+        	"To quit, type 'quit'. To shut down the server type 'shutdown'.\n"
+		. "\nCLIENT:>>> \n";
+
+    
+	socket_write($msgsock, $msg, strlen($msg));
+	
 
     do {
-        if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
+        if (false === ($buf = socket_read($msgsock, $recv_port, PHP_NORMAL_READ))) {
             echo "socket_read() failed: reason: " . socket_strerror(socket_last_error($msgsock)) . "\n";
             break 2;
         }
@@ -49,9 +91,38 @@ do {
             socket_close($msgsock);
             break 2;
         }
-        $talkback = "PHP: You said '$buf'.\n";
+	if($buf == 'help'){
+
+		$output = printMenu();
+			
+	}
+	if($buf == 'indir'){
+		
+		$output = shell_exec('ls');
+		
+	}
+
+	if ($output){
+		$output = setOutput($output);
+	}
+
+
+        $talkback = "\nPHP SERVER RECEIVED: '$buf'.\nCLIENT:>>>\n";
         socket_write($msgsock, $talkback, strlen($talkback));
-        echo "$buf\n";
+	socket_write($msgsock, $output, strlen($output));	
+
+	
+	echo "\nSERVER RECEIVED: \t$buf\t\t" . " from $argv[1]:$recv_port\n";
+
+	
+	
+	//reset output
+	$output = "";
+
+
+	//message on serverside on startup
+	echo "\nSERVER LISTENING: \n";
+	
     } while (true);
     socket_close($msgsock);
 } while (true);
